f36fc48b1190e0267e0d7b596f44ecf5040e56e0
Trofimov Eugene <evgentrofi@gmail.com>
2019-07-03 18:35:38 +0800

    Добавьте файлы проекта.

---
+++ Pages/Email/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AutoSenderEmail.Models;
+
+namespace AutoSenderEmail.Pages.Email
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly AutoSenderEmail.Models.AutoSenderEmailContext _context;
+
+        public DeleteModel(AutoSenderEmail.Models.AutoSenderEmailContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public EmailModel EmailModel { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            EmailModel = await _context.EmailModel.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (EmailModel == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            EmailModel = await _context.EmailModel.FindAsync(id);
+
+            if (EmailModel != null)
+            {
+                _context.EmailModel.Remove(EmailModel);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
